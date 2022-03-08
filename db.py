22335d9aa4ec4ce7b3654633b0587bf423ece58c
Subash Palvel <79637176+SUBASHPALVEL@users.noreply.github.com>
2022-03-09 05:11:14 +0530

    Add files via upload

---
+++ db.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db):
+        self.con = sqlite3.connect(db)
+        self.cur = self.con.cursor()
+        sql = """
+        CREATE TABLE IF NOT EXISTS employees(
+            id Integer Primary Key,
+            name text,
+            age text,
+            doj text,
+            email text,
+            gender text,
+            contact text,
+            address text
+        )
+        """
+        self.cur.execute(sql)
+        self.con.commit()
+
+    # Insert Function
+    def insert(self, name, age, doj, email, gender, contact, address):
+        self.cur.execute("insert into employees values (NULL,?,?,?,?,?,?,?)",
+                         (name, age, doj, email, gender, contact, address))
+        self.con.commit()
+
+    # Fetch All Data from DB
+    def fetch(self):
+        self.cur.execute("SELECT * from employees")
+        rows = self.cur.fetchall()
+        # print(rows)
+        return rows
+
+    # Delete a Record in DB
+    def remove(self, id):
+        self.cur.execute("delete from employees where id=?", (id,))
+        self.con.commit()
+
+    # Update a Record in DB
+    def update(self, id, name, age, doj, email, gender, contact, address):
+        self.cur.execute(
+            "update employees set name=?, age=?, doj=?, email=?, gender=?, contact=?, address=? where id=?",
+            (name, age, doj, email, gender, contact, address, id))
+        self.con.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+btn_frame = Frame(entries_frame, bg="#535c68")
+btn_frame.grid(row=6, column=0, columnspan=4, padx=10, pady=10, sticky="w")
+btnAdd = Button(btn_frame, command=add_employee, text="Add Details", width=15, font=("Calibri", 16, "bold"), fg="white",
+                bg="#16a085", bd=0).grid(row=0, column=0)
+btnEdit = Button(btn_frame, command=update_employee, text="Update Details", width=15, font=("Calibri", 16, "bold"),
+                 fg="white", bg="#2980b9",
+                 bd=0).grid(row=0, column=1, padx=10)
+btnDelete = Button(btn_frame, command=delete_employee, text="Delete Details", width=15, font=("Calibri", 16, "bold"),
+                   fg="white", bg="#c0392b",
+                   bd=0).grid(row=0, column=2, padx=10)
+btnClear = Button(btn_frame, command=clearAll, text="Clear Details", width=15, font=("Calibri", 16, "bold"), fg="white",
+                  bg="#f39c12",
+                  bd=0).grid(row=0, column=3, padx=10)
+"""
